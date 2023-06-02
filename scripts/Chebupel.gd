f1126cd6b0357776869ffbf42d530c7835397a67
expecto27 <expeo@vk.com>
2023-06-02 17:58:29 +0800

    collition

--- scripts/Chebupel.gd
+++ scripts/Chebupel.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @onready var _animation_player = $AnimationPlayer
 var cur_dir = "none"
 
-var capacity = 3
+var capacity = 2
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -11,7 +11,28 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed('up'):
+	
+	if Input.is_action_pressed("left") and Input.is_action_pressed('up'):
+		cur_dir = "left"
+		play_anim(1)
+		self.position.x -= capacity / 1.5
+		self.position.y -= capacity / 1.5
+	elif Input.is_action_pressed('left') and Input.is_action_pressed('down'):
+		cur_dir = "left"
+		play_anim(1)
+		self.position.x -= capacity / 1.5
+		self.position.y += capacity / 1.5
+	elif Input.is_action_pressed("right") and Input.is_action_pressed('up'):
+		cur_dir = "right"
+		play_anim(1)
+		self.position.x += capacity / 1.5
+		self.position.y -= capacity / 1.5
+	elif Input.is_action_pressed("right") and Input.is_action_pressed('down'):
+		cur_dir = "right"
+		play_anim(1)
+		self.position.x += capacity / 1.5
+		self.position.y += capacity / 1.5
+	elif Input.is_action_pressed('up'):
 		cur_dir = "up"
 		play_anim(1)
 		self.position.y -= capacity
@@ -29,9 +50,10 @@ func _process(delta):
 		self.position.x += capacity
 	else:
 		play_anim(0)
-	
+	move_and_slide()
 	position.x = clamp(position.x, 0, 10000)
 	position.y = clamp(position.y, 0, 10000)
+	
 	
 func play_anim(movement):
 	var dir = cur_dir
@@ -63,3 +85,4 @@ func play_anim(movement):
 			_animation_player.play("w_u")
 		elif movement == 0:
 			_animation_player.play("Idle_up")
+
