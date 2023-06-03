37e19d5dddf3b0d037cc5a20b33a629de9a4c792
expecto27 <expeo@vk.com>
2023-06-03 23:41:40 +0800

    npc city population

--- scripts/woman.gd
+++ scripts/woman.gd
@@ -1,33 +1,24 @@
 extends CharacterBody2D
-var stands = true
-var destination = Vector2()
-var vel = 200
+
+var moveSpeed = 4  # Скорость движения NPC
+
+var stand = 100
+
+
+func _ready():
+	randomize()  # Инициализируем генератор случайных чисел
+
+func _process(delta):
+	if stand == 0:
+		stand = 300
+		var direction = Vector2(randf()-0.5, randf()-0.5).normalized()
+		print(direction)
+		velocity = direction * moveSpeed
+	elif stand < 70:
+		stand -= 1
+		velocity = Vector2()
+	else:
+		stand -= 1
 
 func _physics_process(delta):
-	wander()
-	
-func set_destination(dest):
-	destination = dest
-	self.velocity = (destination - position).normalized() * vel
 	move_and_slide()
-	stands = false
-	
-	
-func cancel_movement():
-	velocity = Vector2()
-	destination = Vector2()
-	$StandingTimer.start(0)
-
-func wander():
-	var pos = position
-	if stands:
-		randomize()
-		var x = int(randf_range(pos.x - 50, pos.x + 50)) * 100
-		var y = int(randf_range(pos.y - 50, pos.y + 50)) * 100
-		set_destination(Vector2(x, y))
-	elif velocity != Vector2():
-		cancel_movement() 
-
-
-func _on_standing_timer_timeout():
-	stands = true
